8904a89fae1beec0128bb8cf598e0846eac0d10f
Brunim1 <bruno.30erik@gmail.com>
2023-10-26 18:51:51 -0300

    firt commit

--- script.js
+++ script.js
@@ -5,31 +5,41 @@ const cover = document.getElementById('cover');
 const play = document.getElementById('play');
 const next = document.getElementById('next');
 const previous = document.getElementById('previous');
+const likeButton = document.getElementById('like');
 const currentProgress = document.getElementById('current-progress');
 const progressContainer = document.getElementById('progress-container');
 const shuffleButton = document.getElementById('shuffle');
 const repeatButton = document.getElementById('repeat');
+const songTime = document.getElementById('song-time');
+const totalTime = document.getElementById('total-time');
+
 
 
 const leaveThe = {
     songName : 'Leave The Door Open',
     artist : 'Bruno Mars',
-    file: 'leave_the_ door_open'
+    file: 'leave_the_ door_open',
+    img: 'bmars.jpg',
+    liked: false,
 };
 const whatGoes = {
     songName : 'What Goes Around Comes Around',
     artist : 'Justim Timberlake',
-    file: 'what_goes_around_comes_around'
+    file: 'what_goes_around_comes_around',
+    img: 'jtimber.jpg',
+    liked: false,
 };
 const whateverIt = {
     songName : 'Whatever It Takes',
     artist : 'Imagine Dragons',
-    file: 'whatever_it_takes'
+    file: 'whatever_it_takes',
+    img: 'wdragon.jpg',
+    liked: false,
 };
 let isPlaying = false;
 let isShuffled = false;
 let repeatOn = false;
-const originalPlaylist = [leaveThe, whatGoes, whateverIt];
+const originalPlaylist = JSON.parse(localStorage.getItem('playlist')) ?? [leaveThe, whatGoes, whateverIt];
 let sortedPlaylist = [...originalPlaylist];
 let index = 0;
 
@@ -56,11 +66,25 @@ function playPauseDecider(){
     }
 }
 
+function likeButtonRender() {
+    if(sortedPlaylist[index].liked === true) {
+        likeButton.querySelector('.bi').classList.remove('bi-heart');
+        likeButton.querySelector('.bi').classList.add('bi-heart-fill');
+        likeButton.classList.add('button-active-like');
+    }
+    else {
+        likeButton.querySelector('.bi').classList.add('bi-heart');
+        likeButton.querySelector('.bi').classList.remove('bi-heart-fill');
+        likeButton.classList.remove('button-active-like');
+    }
+}
+
 function initializeSong(){
-    cover.src = `images/${sortedPlaylist[index].file}.jpg`;
+    cover.src = `images/${sortedPlaylist[index].img}`;
     song.src = `songs/${sortedPlaylist[index].file}.mp3`;
     songName.innerText = sortedPlaylist[index].songName;
     bandName.innerText = sortedPlaylist[index].artist;
+    likeButtonRender();
 }
 
 function previousSong(){
@@ -85,9 +109,10 @@ function nextSong(){
     playSong();
 }
 
-function updateProgressBar(){
+function updateProgress(){
     const barWidth = (song.currentTime/song.duration)*100;
     currentProgress.style.setProperty('--progress', `${barWidth}%`);
+    songTime.innerText = toHHMMSS(song.currentTime);
 }
 
 function jumpTo(event){
@@ -141,13 +166,44 @@ function nextOrRepeat(){
     }
 }
 
+function toHHMMSS(originalNumber){
+    let hours = Math.floor(originalNumber / 3600);
+    let min = Math.floor((originalNumber - hours * 3600) / 60);
+    let secs = Math.floor(originalNumber - hours * 3600 - min * 60);
+    
+    
+    return `${hours.toString().padStart(2, '0')}:${min
+        .toString()
+        .padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+} 
+
+function updateTotalTime(){
+    totalTime.innerText = toHHMMSS(song.duration);
+}
+
+function likeButtonClicked() {
+    if(sortedPlaylist[index].liked === false) {
+        sortedPlaylist[index].liked = true;
+    } else {
+        sortedPlaylist[index].liked = false;
+    }
+    likeButtonRender();
+    localStorage.setItem('playlist', JSON.stringify (originalPlaylist));
+    
+    
+    
+    ;
+}
+
 initializeSong();
 
 play.addEventListener('click', playPauseDecider);
 previous.addEventListener('click', previousSong);
 next.addEventListener('click', nextSong);
-song.addEventListener('timeupdate', updateProgressBar);
+song.addEventListener('timeupdate', updateProgress);
 song.addEventListener('ended', nextOrRepeat);
+song.addEventListener('loadedmetadata', updateTotalTime)
 progressContainer.addEventListener('click', jumpTo);
 shuffleButton.addEventListener('click', shuffleButtonClicked);
 repeatButton.addEventListener('click', repeatButtonClicked);
+likeButton.addEventListener('click', likeButtonClicked);
